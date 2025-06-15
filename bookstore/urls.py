6f38658d0b6ae3c47318adb6f7c046fab0aa4408
Adriano <adrianomarque135@gmail.com>
2025-06-14 23:31:45 -0300

    configuraçoes pythonanywhere

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 import debug_toolbar
 from django.contrib import admin
+from bookstore import views
 from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('__debug__', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
     re_path(r'bookstore/(?P<version>(v1|v2))/', include('order.urls')),
     re_path(r'bookstore/(?P<version>(v1|v2))/', include('product.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
